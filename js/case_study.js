66cae017ea7959b1a36f50cbb976541ab5929458
Meenakshi Kunduru <meenakunduru@gmail.com>
2019-06-21 20:50:58 -0400

    Pin slideshow to the end of the case study page

--- js/case_study.js
+++ js/case_study.js
@@ -28,15 +28,19 @@ $(document).ready(function(){
 });
 
 $(window).on("load", function(){
-	slideshow = $('.case-study-slideshow').detach();
-	slideshow.appendTo('.container');
+	console.log("Window is loaded and slideshow is joined at the bottom");
+	
+	setTimeout(function(){
+		slideshow = $('.case-study-slideshow').detach();
+		slideshow.appendTo('.container');
 
-	$('.next-project-slides').slick({
-		dots: true,
-		infinite: true,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		prevArrow: '<img alt="Left Arrow" class="slick-prev img-left" src="img/left_arrow.png">',
-		nextArrow: '<img alt="Right Arrow" class="slick-next img-right" src="img/right_arrow.png">'
-	});
+		$('.next-project-slides').slick({
+			dots: true,
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: '<img alt="Left Arrow" class="slick-prev img-left" src="img/left_arrow.png">',
+			nextArrow: '<img alt="Right Arrow" class="slick-next img-right" src="img/right_arrow.png">'
+		});
+	}, 2000);
 });
