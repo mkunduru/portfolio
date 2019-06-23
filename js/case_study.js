48601aa11c04c7c1b3f42d8c401255cf87435869
Meenakshi Kunduru <meenakunduru@gmail.com>
2019-06-23 17:08:46 -0400

    Add hover to all case study buttons

--- js/case_study.js
+++ js/case_study.js
@@ -11,6 +11,15 @@ $(document).ready(function(){
 		}
 	});
 
+	$('.next-project-slides').slick({
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<img alt="Left Arrow" class="slick-prev img-left" src="img/left_arrow.png">',
+		nextArrow: '<img alt="Right Arrow" class="slick-next img-right" src="img/right_arrow.png">'
+	});
+
 	$.scrollify({
 		section: ".scroll-section",
 		scrollSpeed: 1200,
@@ -25,22 +34,4 @@ $(document).ready(function(){
 			}
 		}
 	});
-});
-
-$(window).on("load", function(){
-	console.log("Window is loaded and slideshow is joined at the bottom");
-	
-	setTimeout(function(){
-		slideshow = $('.case-study-slideshow').detach();
-		slideshow.appendTo('.container');
-
-		$('.next-project-slides').slick({
-			dots: true,
-			infinite: true,
-			slidesToShow: 1,
-			slidesToScroll: 1,
-			prevArrow: '<img alt="Left Arrow" class="slick-prev img-left" src="img/left_arrow.png">',
-			nextArrow: '<img alt="Right Arrow" class="slick-next img-right" src="img/right_arrow.png">'
-		});
-	}, 2000);
 });
